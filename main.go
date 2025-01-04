55574de2d9ca348dede11c647b13279811ce3232
Sergei Kondurian <skondurian@gmail.com>
2025-01-03 17:15:54 -0800

    switch from 3rd party go-arg to stdlib flag

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-
-	"github.com/alexflint/go-arg"
+	"strings"
 )
 
 // global vars
@@ -13,26 +13,29 @@ var ARGS Args
 //
 
 type Args struct {
-	Command       string   `arg:"positional" help:"command to run on remote hosts"`
-	Hosts         string   `help:"read hosts from the given host_file" placeholder:"host_file"`
-	Host          []string `arg:"-H" help:"add the given host strings to the list of hosts" placeholder:"host..."`
-	User          string   `arg:"-l" help:"use the given username as the default for any host entries that don't specifically specify a user"`
-	Key           string   `arg:"-k,required" help:"SSH private key"`
-	Par           int      `arg:"-p" help:"use the given number as the maximum number of concurrent connections"`
-	Timeout       int      `arg:"-t" help:"make connections time out after the given number of seconds (unless set to 0)"`
-	Outdir        string   `arg:"-o" help:"save standard output to files in the given directory"`
-	Errdir        string   `arg:"-e" help:"save standard error to files in the given directory"`
-	Inline        bool     `arg:"-i" help:"display standard output and standard error as each host completes"`
-	Inline_Stdout bool     `help:"display standard output (but not standard error) as each host completes"`
-	Send_Input    bool     `arg:"-I" help:"read input and send to each ssh process"`
+	Command  string
+	Hostfile string
+	Hosts    string
+	User     string
+	Key      string
+	// Timeout       int
+	// Outdir        string
+	// Errdir        string
+	// Inline        bool
+	// Inline_Stdout bool
+	Send_Input bool
 }
 
 func main() {
-	parser := arg.MustParse(&ARGS)
-	if len(os.Args) == 1 {
-		// no arguments were provided
-		parser.WriteHelp(os.Stderr)
-		return
+	// parser := arg.MustParse(&ARGS)
+	// if len(os.Args) == 1 {
+	// 	// no arguments were provided
+	// 	parser.WriteHelp(os.Stderr)
+	// 	return
+	// }
+	if err := parse_args(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 	if err := verify_args(&ARGS); err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -43,13 +46,24 @@ func main() {
 	os.Exit(gopssh.Main())
 }
 
+func parse_args() error {
+	flags := flag.NewFlagSet("gopssh", flag.ExitOnError)
+	flags.StringVar(&ARGS.Hostfile, "host_file", "", "read hosts from the given host_file")
+	flags.StringVar(&ARGS.Hosts, "hosts", "", "add the given host strings to the list of hosts")
+	flags.StringVar(&ARGS.User, "user", "", "use the given username")
+	flags.StringVar(&ARGS.Key, "key", "", "SSH private key to authenticate with")
+	err := flags.Parse(os.Args[1:])
+	ARGS.Command = strings.Join(flags.Args(), " ")
+	return err
+}
+
 func verify_args(args *Args) error {
 	switch {
 	case args.Command == "" && !args.Send_Input:
 		return &EInvalidArgs{"please provide command or -I"}
 	case args.Command != "" && args.Send_Input:
 		return &EInvalidArgs{"please provide either command or -I, not both"}
-	case args.Hosts == "" && len(args.Host) == 0:
+	case args.Hostfile == "" && len(args.Hosts) == 0:
 		return &EInvalidArgs{"please provide --host or/and --hosts"}
 	default:
 		return nil
